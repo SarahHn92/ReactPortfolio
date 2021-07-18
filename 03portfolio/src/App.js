a899c33ca6893ee6fce7ad2bb3a68c7c668e59c6
Sarah Hinton <smcfarland2@gmail.com>
2021-07-18 21:18:39 +1000

    first commit

---
+++ 03portfolio/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Skills from './components/Skills';
+import About from "./components/About";
+import Projects from './components/Projects';
+
+function App() {
+    return <Header />;
+}
+
+export default App;
