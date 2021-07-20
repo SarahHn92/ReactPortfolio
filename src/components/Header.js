78567a332c5605df3ec645623be858f305c4c0bc
Sarah Hinton <smcfarland2@gmail.com>
2021-07-20 16:06:50 +1000

    added nav links and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import '../Assets/croppedGum.jpg';
+// import photo from'../Assets/croppedGum.png';
 import '../styles/Header.css';
 import Nav from './Nav';
 
 function Header() {
     return (
         <header className="header">
-            <Nav />
-            <img className="headerImg" src="../Assets/croppedGum.jpg" alt="new growth eucalypt leaves"></img>
+            <div>
+               <Nav /> 
+            </div>
+            {/* <div>
+                <img className="headerImg" src={photo} alt="new growth eucalypt leaves"></img>
+            </div> */}
             <div className="title">
                 <h1 className="t2">Hi.</h1>
                 <h1 className="t2">I'm Sarah McFarland.</h1> 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,17 +3,21 @@ import '../styles/Nav.css';
 
 function Nav(props) {
     return (
-       <nav className="navbar">
-           <div className="hamburger"></div>
-           <div className="logo"><a href="/">"LOGO"</a></div>
-           <div className="nav-items">
-               <ul>
-                   <li className="about-nav"><a href="/About">"About"</a></li>
-                   <li className="skills-nav"><a href="/Skills">"Skills"</a></li>
-                   <li className="projects-nav"><a href="/Projects">"Projects"</a></li>
-               </ul>
-           </div>
-       </nav> 
+        <header className="navbar">
+            <nav className="toolbar">
+                <div className="hamburger"></div>
+                <div className="logo"><a href="/">"LOGO"</a></div>
+                <div className="space"></div>                
+                <div className="nav-items">
+                    <ul>
+                        <li className="about-nav"><a href="/About">About</a></li>
+                        <li className="skills-nav"><a href="/Skills">Skills</a></li>
+                        <li className="projects-nav"><a href="/Projects">Projects</a></li>
+                    </ul>
+                </div>
+            </nav> 
+        </header>
+       
     )
 
 }
