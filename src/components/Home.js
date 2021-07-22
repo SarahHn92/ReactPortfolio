0aa43986a3219a30e346e51b5dcd831710b948f3
Sarah Hinton <smcfarland2@gmail.com>
2021-07-22 18:01:48 +1000

    changing file structure

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import HomeSkills from './components/HomeSkills';
+import HomeAbout from "./components/HomeAbout";
+import HomeProjects from './components/HomeProjects';
+import Footer from './components/Footer.js'
+
+function PortfolioContainer() {
+
+    // var [homePage, sethomePage] = useState(initialState);
+    // const sethomePage = "/";
+    return (
+        <div>
+            <Header />
+            <HomeAbout />
+            <HomeSkills />
+            <HomeProjects />
+            <Footer />
+        </div>
+    ) ;
+}
+
+export default PortfolioContainer;
